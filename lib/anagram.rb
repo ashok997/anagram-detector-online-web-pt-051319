44e3ab33f5ba350d2f0f12b26e689edb4917debd
ASHOK MAHARJAN <ashok997@gmail.com>
2019-06-06 01:57:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :sample
+  
+  def initialize(sample)
+    @sample = sample
+  end
+  
+  def match(test_array)
+  # binding.pry
+ # test_array.each do |i| 
+    test_array[0].split('').sort.equal?(@sample.split('').sort)
+ # end
+  end
+
+end
